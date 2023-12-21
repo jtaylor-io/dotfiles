9c23e1adaa45f5db807344d443f4dc32969b0e73
John Taylor <johnmichaeltaylor79@gmail.com>
2023-12-21 09:59:44 +0000

    Add orgmode

---
+++ nvim/.config/nvim/lua/jtaylor-io/plugins/orgmode.lua
@@ -0,0 +1,31 @@
+return {
+	"nvim-orgmode/orgmode",
+	dependencies = {
+		{ "nvim-treesitter/nvim-treesitter", lazy = true },
+	},
+	event = "VeryLazy",
+	config = function()
+		-- Load treesitter grammar for org
+		require("orgmode").setup_ts_grammar()
+
+		-- Setup treesitter
+		require("nvim-treesitter.configs").setup({
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = { "org" },
+			},
+			ensure_installed = { "org" },
+		})
+
+		-- Setup orgmode
+		require("orgmode").setup({
+			org_agenda_files = "~/Dropbox/org/**/*",
+			org_default_notes_file = "~/Dropbox/org/refile.org",
+		})
+		require("cmp").setup({
+			sources = {
+				{ name = "orgmode" },
+			},
+		})
+	end,
+}
