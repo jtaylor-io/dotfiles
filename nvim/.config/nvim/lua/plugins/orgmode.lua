518fc10385c497841d38afecf5abe6c0e2ff19bc
John Taylor <johnmichaeltaylor79@gmail.com>
2024-01-12 20:42:05 +0000

    sorted out cmp sources

--- nvim/.config/nvim/lua/plugins/orgmode.lua
+++ nvim/.config/nvim/lua/plugins/orgmode.lua
@@ -1,32 +1,17 @@
 return {
 	"nvim-orgmode/orgmode",
 	dependencies = {
-		{ "nvim-treesitter/nvim-treesitter", lazy = true },
+		{ "nvim-treesitter/nvim-treesitter" },
 	},
 	event = "VeryLazy",
 	config = function()
 		-- Load treesitter grammar for org
 		require("orgmode").setup_ts_grammar()
 
-		-- Setup treesitter
-		require("nvim-treesitter.configs").setup({
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = { "org" },
-			},
-			ensure_installed = { "org" },
-		})
-
 		-- Setup orgmode
 		require("orgmode").setup({
 			org_agenda_files = "~/Dropbox/org/**/*",
 			org_default_notes_file = "~/Dropbox/org/refile.org",
 		})
-		require("cmp").setup({
-			sources = {
-				{ name = "orgmode" },
-			},
-		})
 	end,
 }
-
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,110 +1,110 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        -- ensure_installed = {
-        --   "bash",
-        --   "c",
-        --   "go",
-        --   "javascript",
-        --   "lua",
-        --   "markdown",
-        --   "markdown_inline",
-        --   "org",
-        --   "query",
-        --   "regex",
-        --   "rust",
-        --   "sql",
-        --   "typescript",
-        --   "vim",
-        --   "vimdoc",
-        -- },
-        -- Install parsers synchronously (only applied to `ensure_installed`)
-        sync_install = false,
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				-- ensure_installed = {
+				--   "bash",
+				--   "c",
+				--   "go",
+				--   "javascript",
+				--   "lua",
+				--   "markdown",
+				--   "markdown_inline",
+				--   "org",
+				--   "query",
+				--   "regex",
+				--   "rust",
+				--   "sql",
+				--   "typescript",
+				--   "vim",
+				--   "vimdoc",
+				-- },
+				-- Install parsers synchronously (only applied to `ensure_installed`)
+				sync_install = false,
 
-        -- Automatically install missing parsers when entering buffer
-        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-        auto_install = true,
+				-- Automatically install missing parsers when entering buffer
+				-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+				auto_install = true,
 
-        indent = { enable = true },
-        highlight = {
-          enable = true,
+				indent = { enable = true },
+				highlight = {
+					enable = true,
 
-          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-          -- Using this option may slow down your editor, and you may see some duplicate highlights.
-          -- Instead of true it can also be a list of languages
-          additional_vim_regex_highlighting = false,
-        },
+					-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+					-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+					-- Using this option may slow down your editor, and you may see some duplicate highlights.
+					-- Instead of true it can also be a list of languages
+					additional_vim_regex_highlighting = { "org" },
+				},
 
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<c-space>",
-            node_incremental = "<c-space>",
-            scope_incremental = "<c-s>",
-            node_decremental = "<leader>b",
-          },
-        },
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-            keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
-              ["aa"] = "@parameter.outer",
-              ["ia"] = "@parameter.inner",
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
-              ["ii"] = "@conditional.inner",
-              ["ai"] = "@conditional.outer",
-              ["il"] = "@loop.inner",
-              ["al"] = "@loop.outer",
-              ["at"] = "@comment.outer",
-            },
-          },
-          move = {
-            enable = true,
-            set_jumps = true, -- whether to set jumps in the jumplist
-            goto_next_start = {
-              ["]m"] = "@function.outer",
-              ["]]"] = "@class.outer",
-            },
-            goto_next_end = {
-              ["]M"] = "@function.outer",
-              ["]["] = "@class.outer",
-            },
-            goto_previous_start = {
-              ["[m"] = "@function.outer",
-              ["[["] = "@class.outer",
-            },
-            goto_previous_end = {
-              ["[M"] = "@function.outer",
-              ["[]"] = "@class.outer",
-            },
-            -- goto_next = {
-            --   [']i'] = "@conditional.inner",
-            -- },
-            -- goto_previous = {
-            --   ['[i'] = "@conditional.inner",
-            -- }
-          },
-          swap = {
-            enable = true,
-            swap_next = {
-              ["<leader>a"] = "@parameter.inner",
-            },
-            swap_previous = {
-              ["<leader>A"] = "@parameter.inner",
-            },
-          },
-        },
-      })
-    end,
-  },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<c-space>",
+						node_incremental = "<c-space>",
+						scope_incremental = "<c-s>",
+						node_decremental = "<leader>b",
+					},
+				},
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+						keymaps = {
+							-- You can use the capture groups defined in textobjects.scm
+							["aa"] = "@parameter.outer",
+							["ia"] = "@parameter.inner",
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+							["ii"] = "@conditional.inner",
+							["ai"] = "@conditional.outer",
+							["il"] = "@loop.inner",
+							["al"] = "@loop.outer",
+							["at"] = "@comment.outer",
+						},
+					},
+					move = {
+						enable = true,
+						set_jumps = true, -- whether to set jumps in the jumplist
+						goto_next_start = {
+							["]m"] = "@function.outer",
+							["]]"] = "@class.outer",
+						},
+						goto_next_end = {
+							["]M"] = "@function.outer",
+							["]["] = "@class.outer",
+						},
+						goto_previous_start = {
+							["[m"] = "@function.outer",
+							["[["] = "@class.outer",
+						},
+						goto_previous_end = {
+							["[M"] = "@function.outer",
+							["[]"] = "@class.outer",
+						},
+						-- goto_next = {
+						--   [']i'] = "@conditional.inner",
+						-- },
+						-- goto_previous = {
+						--   ['[i'] = "@conditional.inner",
+						-- }
+					},
+					swap = {
+						enable = true,
+						swap_next = {
+							["<leader>a"] = "@parameter.inner",
+						},
+						swap_previous = {
+							["<leader>A"] = "@parameter.inner",
+						},
+					},
+				},
+			})
+		end,
+	},
 }
